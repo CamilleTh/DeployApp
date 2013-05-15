cef0aafec5f5bec6625db39bc5f28b1cc2803601
camille <camille.thomassin@intech.lu>
2013-05-15 17:03:31 +0200

    0.0.4

---
+++ src/main/java/migration/Migrate.java
@@ -0,0 +1,53 @@
+package migration;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.flyway.core.Flyway;
+import com.googlecode.flyway.core.api.MigrationVersion;
+
+/**
+ * Servlet implementation class Migrate
+ */
+public class Migrate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Migrate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  // Create the Flyway instance
+        String version = null;
+		request.setAttribute("V", version);
+		
+		if(version != null){
+			Flyway flyway = new Flyway();
+	        // Point it to the database
+	        flyway.setDataSource("jdbc:mysql://mysql1.alwaysdata.com/40853_intech", "40853_2", "intech");
+	        flyway.clean();
+	        
+	        flyway.setTarget(new MigrationVersion(version));
+	        // Start the migration
+	        flyway.migrate();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
