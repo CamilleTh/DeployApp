1038f4ae99d954523dd220f4e7360dac7ad9673b
camille <camille.thomassin@intech.lu>
2013-05-15 17:22:36 +0200

    0.0.5

--- src/main/resources/db/migration/V1__CreateTablePersonne.sql
+++ src/main/resources/db/migration/V1__CreateTablePersonne.sql
@@ -1,20 +1,7 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Table `Personne`
--- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Personne` (
   `idPersonne` INT NOT NULL AUTO_INCREMENT ,
   `prenom` VARCHAR(45) NULL ,
   `nom` VARCHAR(45) NULL ,
   `age` VARCHAR(45) NULL ,
   `adresse` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idPersonne`) )
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+  PRIMARY KEY (`idPersonne`) );
